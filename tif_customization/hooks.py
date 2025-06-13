1a24e82e3460b2d584a3b1f7703f3500d1582b71
Mohtashim-1 <shoaibmohtashin973@gmail.com>
2025-06-13 18:33:43 +0500

    chore: fix in hooks.py

--- tif_customization/hooks.py
+++ tif_customization/hooks.py
@@ -140,7 +140,7 @@ app_license = "mit"
 doc_events = {
 	"Delivery Note": {
 		"on_update": "tif_customization.tif_customization.doctype.delivery_note.delivery_note.get_rate_from_courier",
-        "on_update": "tif_customization.tif_customization.doctype.delivery_note.delivery_note.on_update"
+        "on_submit": "tif_customization.tif_customization.doctype.delivery_note.delivery_note.on_update"
 
 	}
 }
